c46184b9143628415a49d8e9028107776e8623e1
syedshaikbaji <91525914+syedshaikbaji@users.noreply.github.com>
2023-04-26 20:53:40 +0530

    Add files via upload
    replace

--- actions-demo-scripts.js
+++ actions-demo-scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     $('.days li').on('click', function () {
-        // $('.event-list-items')
         $(this).siblings().removeClass('clicked');
         $(this).addClass('clicked');
+        
         if ($(this).find('span').hasClass('event-block')) {
-            $('.events-list-block .events-list-has-data').show();
-            $('.events-list-block .events-list-no-data').hide();
+            $('.events-list-block .events-list-has-data').removeClass('d-none');
+            $('.events-list-block .events-list-no-data').addClass('d-none');
         } else {
-            $('.events-list-block .events-list-no-data').show();
-            $('.events-list-block .events-list-has-data').hide();
+            $('.events-list-block .events-list-no-data').removeClass('d-none');
+            $('.events-list-block .events-list-has-data').addClass('d-none');
         }
     });
 });
